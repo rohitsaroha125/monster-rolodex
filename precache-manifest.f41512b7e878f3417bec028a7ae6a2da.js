e2fd7cd72069a71318bc79938731210762d97821
rohit saroha <rohitsaroha@rohits-MacBook-Air.local>
2020-03-29 11:31:02 +0530

    Updates

---
+++ precache-manifest.f41512b7e878f3417bec028a7ae6a2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75e227a907863563f67fcdfeb9b94e5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "604e4d62867dfad8c251",
+    "url": "/monster-rolodex/static/css/main.ef2907db.chunk.css"
+  },
+  {
+    "revision": "1b94577a15796de21e98",
+    "url": "/monster-rolodex/static/js/2.aa108c56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.aa108c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604e4d62867dfad8c251",
+    "url": "/monster-rolodex/static/js/main.82fc8fb2.chunk.js"
+  },
+  {
+    "revision": "e0b2de5c8d072989a642",
+    "url": "/monster-rolodex/static/js/runtime-main.304f0728.js"
+  }
+]);
